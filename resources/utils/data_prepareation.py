ad2ef522dbb0cf61dc4d56eaacc2141ed747fed3
Nguyen Huu Dat <datttdayne@gmail.com>
2024-06-23 16:29:31 +0700

    commit 1

--- resources/utils/data_prepareation.py
+++ resources/utils/data_prepareation.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--data_root',
         type=str,
-        default=r"/kaggle/input/dataset/data/data",
+        default=r"/kaggle/working/data",
         help='Where you want to save the output input_folder')
     # Sample arguments
     parser.add_argument(
