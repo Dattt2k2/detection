51b94b74532edfc42dc257c3e8468ba5120d825c
Nguyen Huu Dat <datttdayne@gmail.com>
2024-06-23 15:13:59 +0700

    commit

--- resources/utils/data_prepareation.py
+++ resources/utils/data_prepareation.py
@@ -134,7 +134,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--data_root',
         type=str,
-        default=r"/work/21013187/SignLanguageRGBD/data/ver2_all_rgb_only",
+        default=r"/kaggle/input/dataset/data/data",
         help='Where you want to save the output input_folder')
     # Sample arguments
     parser.add_argument(
